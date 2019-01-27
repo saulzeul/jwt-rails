4db69549eaa8febef35d8334aff03582d97d5dcb
Saul Espinoza <saul_zeul@hotmail.com>
2019-01-27 10:15:46 -0600

    Gem for works CORS

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module BackendApiIntranet
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # This script works for permit cors 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '192.168.100.9'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
